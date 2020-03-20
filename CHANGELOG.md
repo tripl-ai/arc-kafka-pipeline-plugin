bdb60fd042dc706cbcfc02438be6edd700cf23ff
Mike Seddon <seddonm1@gmail.com>
2020-03-20 20:41:31 +1100

    bump versions

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,16 +1,23 @@
-## Change Log
+# Change Log
 
-# 1.1.0
+## 1.2.0
+
+- update to Kafka 2.4.1
+- update to Spark 2.4.5
+- update to Arc 2.8.0
+- update to Scala 2.12.10
+
+## 1.1.0
 
 - change `KafkaExtract` to base records to extract based on offsets at start of execution rather than waiting polling to record no records in a time window.
 - improved logging for `KafkaExtract`
 
-# 1.0.1
+## 1.0.1
 
 - update to Spark 2.4.4
 - update to Arc 2.0.1
 - update to Scala 2.12.9
 
-# 1.0.0
+## 1.0.0
 
 - initial release.
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,13 +2,13 @@ import sbt._
 
 object Dependencies {
   // versions
-  lazy val sparkVersion = "2.4.4"
+  lazy val sparkVersion = "2.4.5"
 
   // testing
   val scalaTest = "org.scalatest" %% "scalatest" % "3.0.7" % "test,it"
 
   // arc
-  val arc = "ai.tripl" %% "arc" % "2.0.1" % "provided"
+  val arc = "ai.tripl" %% "arc" % "2.8.0" % "provided"
   val typesafeConfig = "com.typesafe" % "config" % "1.3.1" intransitive()  
 
   // spark
@@ -17,9 +17,9 @@ object Dependencies {
   val sparkAvro = "org.apache.spark" %% "spark-avro" % sparkVersion % "provided"
 
   // kafka
-  val kafka = "org.apache.kafka" %% "kafka" % "2.2.1" intransitive()
-  val kafkaClients = "org.apache.kafka" % "kafka-clients" % "2.2.1" intransitive()
-  val sparkSQLKafka = "org.apache.spark" %% "spark-sql-kafka-0-10" % "2.4.3" intransitive()
+  val kafka = "org.apache.kafka" %% "kafka" % "2.4.1" intransitive()
+  val kafkaClients = "org.apache.kafka" % "kafka-clients" % "2.4.1" intransitive()
+  val sparkSQLKafka = "org.apache.spark" %% "spark-sql-kafka-0-10" % sparkVersion intransitive()
 
   // Project
   val etlDeps = Seq(
